ab2e9be1da4a27932a25a530cd96994f094ae96c
ealvess <everton10alves@gmail.com>
2022-08-04 10:27:16 -0300

    Criando as tabelas do banco a partir das entidades

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 public class Cozinha {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
