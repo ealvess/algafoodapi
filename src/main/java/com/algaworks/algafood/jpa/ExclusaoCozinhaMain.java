b56eaac48a236f11364a05109660c6fbdd6129f4
ealvess <everton10alves@gmail.com>
2022-08-04 11:44:48 -0300

    Excluindo um objeto do banco de dados

--- src/main/java/com/algaworks/algafood/jpa/ExclusaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ExclusaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.AlgafoodapiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class ExclusaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodapiApplication.class)
@@ -16,14 +16,11 @@ public class InclusaoCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
-		
-		cadastroCozinha.salvar(cozinha1);
-		cadastroCozinha.salvar(cozinha2);
+		cadastroCozinha.remover(cozinha);
+	
 	}
 	
 }
