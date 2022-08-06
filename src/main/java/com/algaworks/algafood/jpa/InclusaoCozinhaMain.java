9d1b219daceec7e5afb1773f1421dd14a3477303
ealvess <everton10alves@gmail.com>
2022-08-06 07:41:46 -0300

    Modelando e requisitando um Collection Resource com GET - Cozinha e ESTADO

--- src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -23,8 +23,8 @@ public class InclusaoCozinhaMain {
 		Cozinha cozinha2 = new Cozinha();
 		cozinha2.setNome("Japonesa");
 		
-		cozinhas.adicionar(cozinha1);
-		cozinhas.adicionar(cozinha2);
+		cozinhas.salvar(cozinha1);
+		cozinhas.salvar(cozinha2);
 	}
 	
 }
