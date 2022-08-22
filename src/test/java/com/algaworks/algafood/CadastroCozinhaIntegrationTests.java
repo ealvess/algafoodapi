e457fff166dc6586a5558e25c6647050f41de910
ealvess <everton10alves@gmail.com>
2022-08-22 16:55:31 -0300

    Escrevendo testes de integração

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.validation.ConstraintViolationException;
 
@@ -9,40 +11,64 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @SpringBootTest
 class CadastroCozinhaIntegrationTests {
-	
+
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
 
 	@Test
 	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-		//cenário
+		// cenário
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-		
-		//ação
+
+		// ação
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
-		//validação
+
+		// validação
 		assertThat(novaCozinha).isNotNull();
 		assertThat(novaCozinha.getId()).isNotNull();
 	}
-	
+
 	@Test
-	public void deveFalhar_QunadoCadastrarCozinhaSemNome() {
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
-		
-		ConstraintViolationException erroEsperado =
-			      Assertions.assertThrows(ConstraintViolationException.class, () -> {
-			         cadastroCozinha.salvar(novaCozinha);
-			      });
-			   
-			   assertThat(erroEsperado).isNotNull();
+
+		ConstraintViolationException erroEsperado = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			cadastroCozinha.salvar(novaCozinha);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		EntidadeEmUsoException exception = assertThrows(EntidadeEmUsoException.class, () -> {
+			// cenário
+			Long cozinhaId = 1L;
+			// ação
+			cadastroCozinha.excluir(cozinhaId);
+		});
+		exception.printStackTrace();
+		// validação
+		assertThatExceptionOfType(EntidadeEmUsoException.class);
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		CozinhaNaoEncontradaException exception = assertThrows(CozinhaNaoEncontradaException.class, () -> {
+			Long cozinhaId = 10L;
+			cadastroCozinha.excluir(cozinhaId);
+		});
+		exception.printStackTrace();
+		assertThatExceptionOfType(CozinhaNaoEncontradaException.class);
 	}
 
 }
