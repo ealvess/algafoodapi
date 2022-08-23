938dd8e0dfc064d72918f522b274425cbfb43127
ealvess <everton10alves@gmail.com>
2022-08-23 17:44:37 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +30,21 @@ class CadastroCozinhaIT {
 			.get()///fizer uma requisição get no que foi informado acima
 		.then()//então
 			.statusCode(HttpStatus.OK.value());//o codigo de status tem que ser 200
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()//dado que
+			.basePath("/cozinhas")//o caminho é cozinhas
+			.port(port)//na porta 8080
+			.accept(ContentType.JSON)//o accept é json
+		.when()//quando
+			.get()///fizer uma requisição get no que foi informado acima
+		.then()//então
+			.body("", hasSize(4))
+			.body("nome", hasItems("Indiana", "Tailandesa"));//o codigo de status tem que ser 200
 	}
 	
 }
