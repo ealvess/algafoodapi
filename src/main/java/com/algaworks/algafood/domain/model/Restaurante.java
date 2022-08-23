763029b45db2c10f999143fc7fdfc0f9b9d3dd79
ealvess <everton10alves@gmail.com>
2022-08-23 19:40:11 -0300

    Refatorando o código de teste

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -27,7 +27,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -51,8 +50,6 @@ public class Restaurante {
 	
 	@NotNull
 	@PositiveOrZero
-	@Multiplo(numero = 5)
-//	@TaxaFrete
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
