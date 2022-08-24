8aaee393b63f6aa54db090470653803513d86fd2
ealvess <everton10alves@gmail.com>
2022-08-24 16:13:32 -0300

    Usando @JsonIgnoreProperties e Jackson Mixin

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,9 +2,13 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.module.SimpleModule; 
 
 @Component
 public class JacksonMixinModule extends SimpleModule {
@@ -13,6 +17,8 @@ public class JacksonMixinModule extends SimpleModule {
 	
 	public JacksonMixinModule() {
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+		setMixInAnnotation(Cidade.class, CidadeMixin.class);
+		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
 
 }
