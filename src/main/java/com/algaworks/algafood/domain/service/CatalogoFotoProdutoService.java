8f2350dcee1f9eb2bbc5fa03aa58ebb00b9e09ab
Everton Alves <everton10alves@gmail.com>
2022-09-27 00:45:17 -0300

    Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -1,11 +1,16 @@
 package com.algaworks.algafood.domain.service;
 
+import java.io.InputStream;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
+import com.algaworks.algafood.infrastructure.service.storage.LocalFotoStorageService;
 
 @Service
 public class CatalogoFotoProdutoService {
@@ -13,9 +18,31 @@ public class CatalogoFotoProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private LocalFotoStorageService fotoStorageService;
+	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
-		return produtoRepository.save(foto);
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
+		Long restaurnteId = foto.getRestauranteId();
+		Long produtoId = foto.getProduto().getId();
+		String novoNomeArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
+		
+		Optional<FotoProduto> fotoExistente = produtoRepository.findFotoById(restaurnteId, produtoId);
+		if (fotoExistente.isPresent()) {
+			produtoRepository.delete(fotoExistente.get());
+		}
+		foto.setNomeArquivo(novoNomeArquivo);
+		foto = produtoRepository.save(foto);
+		produtoRepository.flush();
+		
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeAquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+		
+		fotoStorageService.armazenar(novaFoto);
+		
+		return foto;
 	}
 	
 }
