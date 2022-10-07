7d7be6d1aaa235ed333707efd22bcef4c23611a1
Everton Alves <everton10alves@gmail.com>
2022-10-07 12:23:24 -0300

    Publicando Domain Events a partir do Aggregate Root

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.service.EnvioEmailService.Mensagem;
 
 
 @Service
@@ -14,22 +13,19 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
-	@Autowired
-	private EnvioEmailService envioEmail;
-	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
 		
-		var mensagem = Mensagem.builder()
-				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-				.corpo("pedido-confirmado.html")
-				.variavel("pedido", pedido)
-				.destinatario(pedido.getCliente().getEmail())
-				.build();
-		
-		envioEmail.enviar(mensagem);
+//		var mensagem = Mensagem.builder()
+//				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+//				.corpo("pedido-confirmado.html")
+//				.variavel("pedido", pedido)
+//				.destinatario(pedido.getCliente().getEmail())
+//				.build();
+//		
+//		envioEmail.enviar(mensagem);
 	}
 	
 	@Transactional
