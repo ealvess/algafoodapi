0cdb7ccf367a934cd7c67101ffef0365053c8d11
Everton Alves <everton10alves@gmail.com>
2022-10-23 17:16:09 -0300

    Adicionando utras diretivas de Cache-Control na resposta HTTP

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedMethods("*");
 //			.allowedOrigins("*")
 //			.maxAge(30)
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		//ao receber um requisição, ele gera um hash(etag) e coloca no cabeçaljo da resposta
+		//porem tambem verifica se o hash da resposta coincide com a etag que esta no if-none-match 
+		//e se for igual retorna um 304 (nada modificado)
+		return new ShallowEtagHeaderFilter();
 	}
 	
 }
