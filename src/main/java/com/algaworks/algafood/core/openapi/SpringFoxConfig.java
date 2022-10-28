c5c80d25c8f3f8a0ea26e4a5a25c636612517602
Everton Alves <everton10alves@gmail.com>
2022-10-28 15:17:45 -0300

    Descrevendo informações da API na documentação

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -3,7 +3,10 @@ package com.algaworks.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,9 +16,19 @@ public class SpringFoxConfig {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
-                .select()
+              .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-                .build();
+                .build()
+              .apiInfo(apiInfo()) ;
+    }
+    
+    public ApiInfo apiInfo() {
+    	return new ApiInfoBuilder()
+    			.title("AlgaFood API")
+    			.description("API aberta para clientes e restaurantes")
+    			.version("1")
+    			.contact(new Contact("Algaworks", "https://www.algaworks.com", "everton10alves@gmail.com"))
+    			.build();
     }
 
 }
