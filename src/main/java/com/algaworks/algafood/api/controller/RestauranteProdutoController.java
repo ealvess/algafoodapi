cc6017b2859c002c59087c57ccc8aba92fd5559f
Everton Alves <everton10alves@gmail.com>
2023-02-18 19:32:26 -0300

    Descrevendo documentação de endpoints de produtos

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.algaworks.algafood.api.assembler.ProdutoInputDisassembler;
 import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
 import com.algaworks.algafood.api.model.ProdutoModel;
 import com.algaworks.algafood.api.model.input.ProdutoInput;
+import com.algaworks.algafood.api.openapi.controller.RestauranteProdutoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
@@ -28,7 +30,7 @@ import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos")
-public class RestauranteProdutoController {
+public class RestauranteProdutoController implements RestauranteProdutoControllerOpenApi {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -45,7 +47,7 @@ public class RestauranteProdutoController {
 	@Autowired
 	private ProdutoInputDisassembler produtoInputDisassembler;
 	
-	@GetMapping
+	@GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<ProdutoModel> listar(@PathVariable Long restauranteId, @RequestParam(required = false) boolean incluirInativos){
 		Restaurante restaruante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
@@ -60,14 +62,14 @@ public class RestauranteProdutoController {
 		return produtoModelAssembler.toCollectionModel(todosProdutos);
 	}
 	
-	@GetMapping("/{produtoId}")
+	@GetMapping(value = "/{produtoId}",  produces = MediaType.APPLICATION_JSON_VALUE)
 	public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
 		
 		return produtoModelAssembler.toModel(produto);
 	}
 	
-	@PostMapping
+	@PostMapping( produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public ProdutoModel adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
