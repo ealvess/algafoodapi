fd692838c8b4497a369c5c42abec754320f9661b
Everton Alves <everton10alves@gmail.com>
2023-02-28 17:10:00 -0300

    Rmovendo modelo de representação inutilizado da documentação

--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteControllerOpenApi.java
@@ -18,6 +18,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Api(tags = "Restaurantes")
 public interface RestauranteControllerOpenApi {
@@ -28,6 +29,8 @@ public interface RestauranteControllerOpenApi {
 	            name = "projecao", paramType = "query", type = "string")
 	})
 	CollectionModel<RestauranteBasicoModel> listar();
+	
+	@ApiIgnore
 	@ApiOperation(value = "Lista restaurantes", hidden = true)
 	CollectionModel<RestauranteApenasNomeModel> listarApenasNomes();
 
