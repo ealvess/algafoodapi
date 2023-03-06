015698e09ea23bbf7d23e4f027e6043d899170ae
Everton Alves <everton10alves@gmail.com>
2023-03-06 15:00:55 -0300

    Gerando documentação das versões da API com SpringFox e Swagger UI

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -71,154 +71,153 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SpringFoxConfig {
 
 	@Bean
-	  public Docket apiDocket() {
+	public Docket apiDocketV1() {
 		var typeResolver = new TypeResolver();
-		
-	    return new Docket(DocumentationType.OAS_30)
-	      .select()
-	        .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-	        .paths(PathSelectors.any())
-	        .build()
-	      .useDefaultResponseMessages(false)
-	      .globalResponses(HttpMethod.GET, globalGetResponseMessages())
-	      .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
-	      .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
-	      .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
-	      .additionalModels(typeResolver.resolve(Problem.class))
-	      .ignoredParameterTypes(ServletRequest.class,
-	    		  URL.class, Uri.class, URLStreamHandler.class, Resource.class, File.class, InputStream.class)
-	      .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
-	      .directModelSubstitute(Links.class, LinksModelOpenApi.class)
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(PagedModel.class, CozinhaModel.class), 
-	    		  CozinhasModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, PedidoResumoModel.class),
-	    		  PedidosModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, CidadeModel.class),
-	    		  CidadesModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, EstadoModel.class),
-	    		  EstadosModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, FormaPagamentoModel.class),
-	    		  FormasPagamentoModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, GrupoModel.class),
-	    		  GruposModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, PermissaoModel.class),
-	    		  PermissoesModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(PagedModel.class, PedidoResumoModel.class), 
-	    		  PedidosResumoModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, ProdutoModel.class),
-	    		  ProdutosModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, RestauranteBasicoModel.class),
-	    		  RestaurantesBasicoModelOpenApi.class))
-	      
-	      .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, UsuarioModel.class),
-	    		  UsuariosModelOpenApi.class))
-	      
-	      .apiInfo(apiInfo())
-	      .tags(new Tag("Cidades", "Gerencia as cidades"),
-					new Tag("Grupos", "Gerencia os grupos de usuários"),
-					new Tag("Cozinhas", "Gerencia as cozinhas"),
-					new Tag("Formas de pagamento", "Gerencia as formas de pagamento"),
-					new Tag("Pedidos", "Gerencia os pedidos"),
-					new Tag("Restaurantes", "Gerencia os restaurantes"),
-					new Tag("Estados", "Gerencia os estados"),
-					new Tag("Produtos", "Gerencia os produtos de restaurantes"),
-					new Tag("Usuarios", "Gerencia os usuarios"),
-					new Tag("Estatísticas", "Estatísticas da algafood"),
-					new Tag("Permissões", "Gerencia as permissões"));
-	    
-	  }
-	
+
+		return new Docket(DocumentationType.OAS_30)
+				.groupName("V1")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+				.paths(PathSelectors.ant("/v1/**"))
+				.build().useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+				.additionalModels(typeResolver.resolve(Problem.class))
+				.ignoredParameterTypes(ServletRequest.class, URL.class, Uri.class, URLStreamHandler.class,
+						Resource.class, File.class, InputStream.class)
+				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(PagedModel.class, CozinhaModel.class), CozinhasModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules
+						.newRule(typeResolver.resolve(Page.class, PedidoResumoModel.class), PedidosModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, CidadeModel.class), CidadesModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, EstadoModel.class), EstadosModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, FormaPagamentoModel.class),
+						FormasPagamentoModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, GrupoModel.class), GruposModelOpenApi.class))
+
+				.alternateTypeRules(
+						AlternateTypeRules.newRule(typeResolver.resolve(CollectionModel.class, PermissaoModel.class),
+								PermissoesModelOpenApi.class))
+
+				.alternateTypeRules(
+						AlternateTypeRules.newRule(typeResolver.resolve(PagedModel.class, PedidoResumoModel.class),
+								PedidosResumoModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, ProdutoModel.class), ProdutosModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, RestauranteBasicoModel.class),
+						RestaurantesBasicoModelOpenApi.class))
+
+				.alternateTypeRules(AlternateTypeRules.newRule(
+						typeResolver.resolve(CollectionModel.class, UsuarioModel.class), UsuariosModelOpenApi.class))
+
+				.apiInfo(apiInfoV1()).tags(new Tag("Cidades", "Gerencia as cidades"),
+						new Tag("Grupos", "Gerencia os grupos de usuários"),
+						new Tag("Cozinhas", "Gerencia as cozinhas"),
+						new Tag("Formas de pagamento", "Gerencia as formas de pagamento"),
+						new Tag("Pedidos", "Gerencia os pedidos"), new Tag("Restaurantes", "Gerencia os restaurantes"),
+						new Tag("Estados", "Gerencia os estados"),
+						new Tag("Produtos", "Gerencia os produtos de restaurantes"),
+						new Tag("Usuarios", "Gerencia os usuarios"),
+						new Tag("Estatísticas", "Estatísticas da algafood"),
+						new Tag("Permissões", "Gerencia as permissões"));
+
+	}
+
+	@Bean
+	public Docket apiDocketV2() {
+		var typeResolver = new TypeResolver();
+
+		return new Docket(DocumentationType.OAS_30)
+				.groupName("V2")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+				.paths(PathSelectors.ant("/v2/**"))
+				.build().useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+				.additionalModels(typeResolver.resolve(Problem.class))
+				.ignoredParameterTypes(ServletRequest.class, URL.class, Uri.class, URLStreamHandler.class,
+						Resource.class, File.class, InputStream.class)
+				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
+
+				.apiInfo(apiInfoV2());
+
+	}
+
 	@Bean
 	public JacksonModuleRegistrar springFoxJacksonConfig() {
 		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
-    
-    public ApiInfo apiInfo() {
-    	return new ApiInfoBuilder()
-    			.title("AlgaFood API")
-    			.description("API aberta para clientes e restaurantes")
-    			.version("1")
-    			.contact(new Contact("Algaworks", "https://www.algaworks.com", "everton10alves@gmail.com"))
-    			.build();
-    }
-    
-    private List<Response> globalGetResponseMessages() {
-    	  return Arrays.asList(
-    	      new ResponseBuilder()
-    	          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-    	          .description("Erro interno do Servidor")
-    	          .representation( MediaType.APPLICATION_JSON )
-                  .apply(getProblemaModelReference())
-    	          .build(),
-    	      new ResponseBuilder()
-    	          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-    	          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
-    	          .build()
-    	  );
-    	}
-    
-    private List<Response> globalPostPutResponseMessages() {
-        return Arrays.asList(
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                .description("Requisição inválida (erro do cliente)")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build(),
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                .description("Erro interno no servidor")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build(),
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-                .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build(),
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-                .description("Requisição recusada porque o corpo está em um formato não suportado")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build()
-        );
-      }
 
-      private List<Response> globalDeleteResponseMessages() {
-        return Arrays.asList(
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                .description("Requisição inválida (erro do cliente)")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build(),
-            new ResponseBuilder()
-                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                .description("Erro interno no servidor")
-                .representation( MediaType.APPLICATION_JSON )
-                .apply(getProblemaModelReference())
-                .build()
-        );
-      }
-      
-      private Consumer<RepresentationBuilder> getProblemaModelReference() {
-    	    return r -> r.model(m -> m.name("Problema")
-    	            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
-    	                    q -> q.name("Problema").namespace("com.algaworks.algafood.api.exceptionhandler")))));
-    	}
-      
-      
+	public ApiInfo apiInfoV1() {
+		return new ApiInfoBuilder().title("AlgaFood API").description("API aberta para clientes e restaurantes")
+				.version("1").contact(new Contact("Algaworks", "https://www.algaworks.com", "everton10alves@gmail.com"))
+				.build();
+	}
+
+	public ApiInfo apiInfoV2() {
+		return new ApiInfoBuilder().title("AlgaFood API").description("API aberta para clientes e restaurantes")
+				.version("2").contact(new Contact("Algaworks", "https://www.algaworks.com", "everton10alves@gmail.com"))
+				.build();
+	}
+
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do Servidor").representation(MediaType.APPLICATION_JSON)
+				.apply(getProblemaModelReference()).build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build());
+	}
+
+	private List<Response> globalPostPutResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+						.description("Requisição inválida (erro do cliente)").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Erro interno no servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.representation(MediaType.APPLICATION_JSON).apply(getProblemaModelReference()).build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+						.description("Requisição recusada porque o corpo está em um formato não suportado")
+						.representation(MediaType.APPLICATION_JSON).apply(getProblemaModelReference()).build());
+	}
+
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+						.description("Requisição inválida (erro do cliente)").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build(),
+				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Erro interno no servidor").representation(MediaType.APPLICATION_JSON)
+						.apply(getProblemaModelReference()).build());
+	}
+
+	private Consumer<RepresentationBuilder> getProblemaModelReference() {
+		return r -> r.model(m -> m.name("Problema").referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+				q -> q.name("Problema").namespace("com.algaworks.algafood.api.exceptionhandler")))));
+	}
 
 }
